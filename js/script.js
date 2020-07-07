b1305b5751b6c55f98df1fe2fde3e6561154b82f
Stefano <stefano.zaccaria29@gmail.com>
2020-07-07 11:08:42 +0200

    html-css complete! mouseenter nav complete!

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-    
+    $('.relative').mouseenter(function (){
+        $('.dropdown', this).show();
+    });
+    $('.relative').mouseleave(function (){
+        $('.dropdown', this).hide();
+    });
 });
