f7a05bf68c9190d6a2f3e8dc4b7dd87757592dec
Stefano <stefano.zaccaria29@gmail.com>
2020-07-07 17:16:16 +0200

    html-css complete

--- js/script.js
+++ js/script.js
@@ -5,26 +5,4 @@ $(document).ready(function(){
     $('.relative').mouseleave(function (){
         $('.dropdown', this).hide();
     });
-
-    $(window).resize(function (){
-        console.log($(window).width()); 
-        if ($(window).width() >= 1200) {
-            $('header').show();
-            $('.navi').show();
-            $('#download').show();
-            $('.ham-menu').hide();
-        } else if ($(window).width() <= 1199) {
-            $('header').hide();
-            $('.navi').hide();
-            $('#download').hide();
-            $('.ham-menu').show(); 
-        } else if ($(window).width() <= 992) {
-            $('header').hide();
-            $('.navi').hide();
-            $('#download').hide();
-            $('.ham-menu').show();
-            $('.title #open-count').hide();
-            $('.info').hide();
-        }
-    })  
 });
