d2e251e2d62ec6373cdfefefab7c3b7ccb2c7e17
Stefano <stefano.zaccaria29@gmail.com>
2020-07-08 15:30:14 +0200

    ex complete

--- js/script.js
+++ js/script.js
@@ -14,36 +14,20 @@ $(document).ready(function(){
         $('#ham-menu').removeClass('active');
     });
 
-    var denaroId = $('#ham-hidden-denaro');
-    var denaroList = $('#ham-denaro-list');
-    var investimentiId = $('#ham-hidden-investimenti');
-    var investimentiList = $('#ham-investimenti-list');
-    var controlloId = $('#ham-hidden-controllo');
-    var controlloList = $('#ham-controllo-list');
-    var altroId = $('#ham-hidden-altro');
-    var altroList = $('#ham-altro-list');
-    var plansId = $('#ham-hidden-plans');
-    var plansList = $('#ham-plans-list');
-    var aziendaId =  $('#ham-hidden-azienda');
-    var aziendaList =  $('#ham-azienda-list');
-    var aiutoId = $('#ham-hidden-aiuto');
-    var aiutoList = $('#ham-aiuto-list');
-    var seguiciId =   $('#ham-hidden-seguici');
-    var seguiciList = $('#ham-seguici-list');
+    dropdownSlide($('#ham-hidden-denaro'),$('#ham-denaro-list'));
+    dropdownSlide($('#ham-hidden-investimenti'),$('#ham-investimenti-list'));
+    dropdownSlide($('#ham-hidden-controllo'),$('#ham-controllo-list'));
+    dropdownSlide($('#ham-hidden-altro'),$('#ham-altro-list'));
+    dropdownSlide($('#ham-hidden-plans'),$('#ham-plans-list'));
+    dropdownSlide($('#ham-hidden-azienda'),$('#ham-azienda-list'));
+    dropdownSlide($('#ham-hidden-aiuto'),$('#ham-aiuto-list'));
+    dropdownSlide($('#ham-hidden-seguici'),$('#ham-seguici-list'));
 
-    dropdownSlide(denaroId,denaroList);
-    dropdownSlide(investimentiId,investimentiList);
-    dropdownSlide(controlloId,controlloList);
-    dropdownSlide(altroId,altroList);
-    dropdownSlide(plansId,plansList);
-    dropdownSlide(aziendaId,aziendaList);
-    dropdownSlide(aiutoId,aiutoList);
-    dropdownSlide(seguiciId,seguiciList);
 });
 
-function dropdownSlide(x,y) {
-    x.click(function(){
-        y.slideToggle();
+function dropdownSlide(mainSlide,hiddenSlide) {
+    mainSlide.click(function(){
+        hiddenSlide.slideToggle();
     });
 }
 
