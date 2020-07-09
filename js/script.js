e6b36034d5febd82a11658522e6056a1a011c870
Stefano <stefano.zaccaria29@gmail.com>
2020-07-09 09:51:12 +0200

    ex complete

--- js/script.js
+++ js/script.js
@@ -15,14 +15,24 @@ $(document).ready(function(){
     });
     
     dropdownSlide($('#ham-hidden-denaro'),$('#ham-denaro-list'),$('#ham-main>.ham-list>.ham-navi li a #denaro-icon'));
+
     dropdownSlide($('#ham-hidden-investimenti'),$('#ham-investimenti-list'),$('#ham-main>.ham-list>.ham-navi li a #investimenti-icon'));
+
     dropdownSlide($('#ham-hidden-controllo'),$('#ham-controllo-list'),$('#ham-main>.ham-list>.ham-navi li a #controllo-icon'));
+
     dropdownSlide($('#ham-hidden-altro'),$('#ham-altro-list'),$('#ham-main>.ham-list>.ham-navi li a #altro-icon'));
+
     dropdownSlide($('#ham-hidden-plans'),$('#ham-plans-list'),$('#ham-main>.ham-list>.ham-navi li a #plans-icon'));
+
     dropdownSlide($('#ham-hidden-azienda'),$('#ham-azienda-list'),$('#ham-main>.ham-list>.ham-navi li a #azienda-icon'));
+
     dropdownSlide($('#ham-hidden-aiuto'),$('#ham-aiuto-list'),$('#ham-main>.ham-list>.ham-navi li a #aiuto-icon'));
+
     dropdownSlide($('#ham-hidden-seguici'),$('#ham-seguici-list'),$('#ham-main>.ham-list>.ham-navi li a #seguici-icon'));
+
 });
+
+// FUNCTION
 
 function dropdownSlide(mainSlide,hiddenSlide,arrow) {
     mainSlide.click(function(){
